6f0437717cd13a543b137f6e3b0943cb89bdaf98
humashaaa <afridakhan2121@gmail.com>
2025-01-21 16:33:10 +0600

    Created user with createAsyncThunk

--- src/redux/features/user/userSlice.js
+++ src/redux/features/user/userSlice.js
@@ -1,9 +1,20 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import auth from '../../firebase/firebase';
 
 const initialState = {
-  name: 'Mir Hussain',
-  email: 'mir@gmail.com',
+  name: '',
+  email: '',
+  isLoading : true,
+  isError : false,
+  error : ""
 };
+
+export const createUser = createAsyncThunk("userSlice/createUser", async({email, password})=>{
+ const data = await createUserWithEmailAndPassword(auth, email, password)
+ console.log(data)
+ return;
+})
 
 const userSlice = createSlice({
   name: 'userSlice',
